81f5c5326aa4c03c3f25a51a7be63fed8efb6d9a
Austin <austin.woodall72@gmail.com>
2020-02-20 01:20:35 -0600

    text input component

---
+++ src/components/TextInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styled from "styled-components";
+
+const Input = styled.TextInput`
+
+`;
+
+export const TextInput = (props) => {
+
+    return (
+        <View>
+            <TextInput />
+        </View>
+    )
+};
